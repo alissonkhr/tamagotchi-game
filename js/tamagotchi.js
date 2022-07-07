d9d82bf6325115eae9864aff25512e19acbeb0ee
alissonkhr <alissonricardo68@gmail.com>
2022-07-06 18:51:14 -0700

    Created pet object

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -1,1 +1,14 @@
-console.log('Tamagotchiiii!');
+const pet = {
+    name: '',
+    hunger: 1,
+    sleepiness: 1,
+    boredom: 1,
+    age: 1
+}
+
+console.log(pet);
+
+// document.getElementById('startButton').addEventListener('click', startGame)
+// document.getElementById('feedMeButton').addEventListener('click', feedPet)
+// document.getElementById('playButton').addEventListener('click', playWithPet)
+// document.getElementById('sleepButton').addEventListener('click', petSleeps)
